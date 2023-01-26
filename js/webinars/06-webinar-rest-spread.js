21010e791a92e6deffd1571bbddc0ad5091e4e5c
SydorenkoKateryna <sydorenko.k.ih@gmail.com>
2023-01-26 14:30:00 +0200

    webinar-06

--- js/webinars/06-webinar-rest-spread.js
+++ js/webinars/06-webinar-rest-spread.js
@@ -1,10 +1,17 @@
 // # Модуль 3 Заняття 6. Деструктуризація та rest/spread
 
 // ## Example 1 - Деструктуризація
-// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
-// незалежних аргументів.
+// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору незалежних аргументів.
 
-function calcBMI(weight, height) {
+// Було
+// function calcBMI(weight, height) {
+//   const numericWeight = Number(weight.replace(',', '.'));
+//   const numericHeight = Number(height.replace(',', '.'));
+//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
+// }
+
+// Стало
+function calcBMI({ weight, height }) {
   const numericWeight = Number(weight.replace(',', '.'));
   const numericHeight = Number(height.replace(',', '.'));
   return Number((numericWeight / numericHeight ** 2).toFixed(1));
@@ -15,7 +22,7 @@ function calcBMI(weight, height) {
 // console.log(calcBMI('68,3', '1.65'));
 // console.log(calcBMI('118,3', '1.95'));
 
-// Очікується
+// Стало
 console.log(
   calcBMI({
     weight: '88,3',
@@ -35,12 +42,20 @@ console.log(
   }),
 );
 
-
 // ## Example 2 - Деструктуризація
-// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
-// незалежних аргументів.
+// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору незалежних аргументів.
 
-function printContactsInfo(names, phones) {
+// Було
+// function printContactsInfo(names, phones) {
+//   const nameList = names.split(',');
+//   const phoneList = phones.split(',');
+//   for (let i = 0; i < nameList.length; i += 1) {
+//     console.log(`${nameList[i]}: ${phoneList[i]}`);
+//   }
+// }
+
+// Стало
+function printContactsInfo({ names, phones }) {
   const nameList = names.split(',');
   const phoneList = phones.split(',');
   for (let i = 0; i < nameList.length; i += 1) {
@@ -54,25 +69,38 @@ function printContactsInfo(names, phones) {
 //   '89001234567,89001112233,890055566377,890055566300',
 // );
 
-// Очікується
+// Стало
 printContactsInfo({
   names: 'Jacob,William,Solomon,Artemis',
   phones: '89001234567,89001112233,890055566377,890055566300',
 });
 
-
 // ## Example 3 - Глибока деструктуризація
-// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
-// незалежних аргументів.
+// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору незалежних аргументів.
 
-function getBotReport(companyName, repairBots, defenceBots) {
+// function getBotReport(companyName, repairBots, defenceBots) {
+//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// }
+
+// Стало
+// A
+// function getBotReport(bots) {
+//   const {
+//     companyName,
+//     bots: { repairBots, defenceBots },
+//   } = bots;
+//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// }
+
+// B
+function getBotReport({ companyName, bots: { repair: repairBots, defence: defenceBots } }) {
   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
 }
 
 // Було
 // console.log(getBotReport('Cyberdyne Systems', 150, 50));
 
-// Очікується
+// Стало
 console.log(
   getBotReport({
     companyName: 'Cyberdyne Systems',
@@ -83,11 +111,8 @@ console.log(
   }),
 ); // "Cyberdyne Systems has 200 bots in stock"
 
-
 // ## Example 4 - Деструктуризація
-// Перепиши функцію так, щоб вона приймала об'єкт параметрів із властивостями
-// `companyName` та `stock` та виводила репорт про кількість товарів на складі будь-якої
-// компанії.
+// Перепиши функцію так, щоб вона приймала об'єкт параметрів із властивостями `companyName` та `stock` та виводила репорт про кількість товарів на складі будь-якої компанії.
 
 // Рішення
 function getStockReport({ companyName, stock }) {
@@ -119,11 +144,8 @@ console.log(
   }),
 ); // "Belacci has 35 item in stock"
 
-
 // ## Example 5 - Операція spread
-// Доповни функцію `createContact(partialContact)` так, щоб вона повертала новий
-// об'єкт контакту з доданими властивостями `id` та `createdAt`, а також `list` зі
-// значенням "default" якщо в `partialContact` немає такої властивості.
+// Доповни функцію `createContact(partialContact)` так, щоб вона повертала новий об'єкт контакту з доданими властивостями `id` та `createdAt`, а також `list` зі значенням "default" якщо в `partialContact` немає такої властивості.
 
 // Рішення
 function createContact(partialContact) {
@@ -153,10 +175,8 @@ function generateId() {
   return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-
 // ## Example 6 - Операція rest
-// Напиши функцію `transformUsername(user)` так, щоб вона повертала новий об'єкт із властивістю
-// `fullName`, замість `firstName` та `lastName`.
+// Напиши функцію `transformUsername(user)` так, щоб вона повертала новий об'єкт із властивістю `fullName`, замість `firstName` та `lastName`.
 
 // Рішення
 function transformUsername({ firstName, lastName, ...otherProps }) {
@@ -167,7 +187,7 @@ function transformUsername({ firstName, lastName, ...otherProps }) {
 }
 
 console.log(
-  transformId({
+  transformUsername({
     id: 1,
     firstName: 'Jacob',
     lastName: 'Mercer',
@@ -177,7 +197,7 @@ console.log(
 );
 
 console.log(
-  transformId({
+  transformUsername({
     id: 2,
     firstName: 'Adrian',
     lastName: 'Cross',
